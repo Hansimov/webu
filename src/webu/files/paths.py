c0e68c708fb5481f372cbc2c4fdb296671014add
Hansimov <591172499@qq.com>
2025-08-31 06:43:47 +0800

    :zap: [Enhance] files.paths: use qs for query and ps for params for less ambiguity, and remove clumsy html-related funcs

--- src/webu/files/paths.py
+++ src/webu/files/paths.py
@@ -1,5 +1,5 @@
 from tclogger import norm_path, logger, logstr
-from urllib.parse import quote, urlparse
+from urllib.parse import quote, urlparse, parse_qs, urlencode
 
 
 WEBU_LIB_ROOT = norm_path(__file__).parents[1]
@@ -16,11 +16,34 @@ def has_suffix(name: str) -> bool:
     return "." in name
 
 
+def filter_qs(
+    qs: str, include_qs: list[str] = None, exclude_qs: list[str] = None
+) -> str:
+    if not qs:
+        return ""
+    if not include_qs and not exclude_qs:
+        return qs
+
+    qs_dict = parse_qs(qs)
+    if include_qs and exclude_qs:
+        filtered_dict = {
+            k: v for k, v in qs_dict.items() if k in include_qs and k not in exclude_qs
+        }
+    elif include_qs:
+        filtered_dict = {k: v for k, v in qs_dict.items() if k in include_qs}
+    elif exclude_qs:
+        filtered_dict = {k: v for k, v in qs_dict.items() if k not in exclude_qs}
+    else:
+        filtered_dict = qs_dict
+    return urlencode(filtered_dict, doseq=True)
+
+
 def url_to_name(
     url: str,
     keep_scheme: bool = False,
     keep_domain: bool = False,
-    keep_params: bool = False,
+    keep_ps: bool = False,
+    keep_qs: bool = False,
     keep_anchor: bool = False,
     keep_slash: bool = False,
     append_html: bool = False,
@@ -28,10 +51,13 @@ def url_to_name(
     """<scheme>://<netloc>/<path>;<params>?<query>#<fragment>"""
     parsed = urlparse(url)
     p = parsed.path
-    params = parsed.query
+    ps = parsed.params
+    qs = parsed.query
     anchor = parsed.fragment
-    if keep_params and params:
-        p += "?" + params
+    if keep_ps and ps:
+        p += ";" + ps
+    if keep_qs and qs:
+        p += "?" + qs
     if keep_anchor and anchor:
         p += "#" + anchor
     if not keep_slash and p.endswith("/"):
@@ -44,7 +70,6 @@ def url_to_name(
         p = f"{parsed.netloc}/{p}"
     if keep_scheme:
         p = f"{parsed.scheme}://{p}"
-
     return xquote(p)
 
 
@@ -52,11 +77,13 @@ def url_to_segs(
     url: str,
     keep_scheme: bool = False,
     keep_domain: bool = False,
-    keep_params: bool = False,
+    keep_ps: bool = False,
+    keep_qs: bool = False,
     keep_anchor: bool = False,
     keep_slash: bool = False,
     append_html: bool = False,
-    seg_params: bool = False,
+    seg_ps: bool = False,
+    seg_qs: bool = False,
     seg_anchor: bool = False,
 ) -> list[str]:
     """<scheme>://<netloc>/<path>;<params>?<query>#<fragment>"""
@@ -87,14 +114,21 @@ def url_to_segs(
         path_segs[-1] = pseg
     segs.extend(path_segs)
 
-    params = parsed.query
+    ps = parsed.params
+    qs = parsed.query
     anchor = parsed.fragment
-    if keep_params and params:
-        params_str = "?" + params
-        if seg_params:
-            segs.append(params_str)
+    if keep_ps and ps:
+        ps_str = ";" + ps
+        if seg_ps:
+            segs.append(";" + ps_str)
         else:
-            segs[-1] += params_str
+            segs[-1] += ";" + ps_str
+    if keep_qs and qs:
+        qs_str = "?" + qs
+        if seg_qs:
+            segs.append(qs_str)
+        else:
+            segs[-1] += qs_str
     if keep_anchor and anchor:
         anchor_str = "#" + anchor
         if seg_anchor:
@@ -105,54 +139,14 @@ def url_to_segs(
     return [xquote(seg) for seg in segs if seg]
 
 
-def url_to_folder(url: str) -> str:
+def url_to_domain(url: str) -> str:
     domain = urlparse(url).netloc
     return xquote(domain)
 
 
-def url_to_html_name(
-    url: str, keep_domain: bool = True, keep_params: bool = True
-) -> str:
-    return url_to_name(
-        url,
-        keep_scheme=False,
-        keep_domain=keep_domain,
-        keep_params=keep_params,
-        keep_anchor=False,
-        keep_slash=False,
-        append_html=False,
-    )
-
-
-def url_to_folder_and_html_name(url: str) -> tuple[str, str]:
-    folder_name = url_to_folder(url)
-    html_name = url_to_html_name(url, keep_domain=False)
-    return folder_name, html_name
-
-
-def url_to_html_segs(
-    url: str,
-    keep_domain: bool = True,
-    keep_params: bool = True,
-    keep_anchor: bool = False,
-    seg_params: bool = True,
-    seg_anchor: bool = True,
-) -> list[str]:
-    return url_to_segs(
-        url,
-        keep_scheme=False,
-        keep_domain=keep_domain,
-        keep_params=keep_params,
-        keep_anchor=keep_anchor,
-        keep_slash=False,
-        append_html=False,
-        seg_params=seg_params,
-        seg_anchor=seg_anchor,
-    )
-
-
 def test_url_to_html_name():
     urls = [
+        "scheme://netloc/path;params?query#fragment",
         "https://docs.python.org/3.14/whatsnew/3.14.html#incompatible-changes",
         "https://github.com/vllm-project/vllm/blob/main/docs/serving/offline_inference.md#ray-data-llm-api",
         "https://docs.vllm.ai/en/latest/examples/online_serving/api_client.html#api-client",
@@ -160,14 +154,23 @@ def test_url_to_html_name():
     ]
     for url in urls:
         logger.note(f"> {url}")
-        folder, html_name = url_to_folder_and_html_name(url)
+        folder = url_to_domain(url)
+        html_name = url_to_name(
+            url,
+            keep_domain=False,
+            keep_ps=True,
+            keep_qs=True,
+            keep_anchor=False,
+            keep_slash=False,
+            append_html=True,
+        )
         logger.mesg(f"  * {folder} / {logstr.okay(html_name)}")
         segs = url_to_segs(
             url,
             keep_domain=True,
-            keep_params=True,
+            keep_qs=True,
             keep_anchor=True,
-            seg_params=True,
+            seg_qs=True,
             seg_anchor=False,
         )
         segs_str = " / ".join(segs)
