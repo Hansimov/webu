fd6b7f6a11d8f6fd7fd25728223916e6b6fb8752
Hansimov <591172499@qq.com>
2025-10-13 00:25:03 +0800

    :zap: [Enhance] UrlPager: add new args of load_original and keep_original, moduralize load_html_str and save_html_str, and refactor steps in fetch_url

--- src/webu/workers/page.py
+++ src/webu/workers/page.py
@@ -14,25 +14,52 @@ class UrlPager:
         self.client = client or ChromeClientByConfig()
         self.verbose = verbose
 
+    def load_html_str(self, p: Path) -> str:
+        logger.note(f"> Load html from:", verbose=self.verbose)
+        with open(p, "r", encoding="utf-8") as f:
+            s = f.read()
+        logger.mesg(f"  * {p}", verbose=self.verbose)
+        return s
+
+    def save_html_str(self, s: str, p: Path):
+        p.parent.mkdir(parents=True, exist_ok=True)
+        logger.note(f"> Save html to:", verbose=self.verbose)
+        with open(p, "w", encoding="utf-8") as f:
+            f.write(s)
+        logger.okay(f"  + {p}", verbose=self.verbose)
+
     def fetch_url(
         self,
         url: str,
         output_path: PathType,
         output_format: Literal["markdown", "html"] = "html",
+        load_original: bool = True,
+        keep_original: bool = True,
+        is_purify: bool = True,
     ):
-        self.client.start_client()
-        html_str = self.client.get_url_html(url)
-        pure_html_str = purify_html_str(
-            html_str, url=url, keep_href=True, output_format=output_format
-        )
         if not output_path:
             output_path = url_to_path(url=url)
-        output_path.parent.mkdir(parents=True, exist_ok=True)
-        logger.note(f"> Save fetched content to:", verbose=self.verbose)
-        with open(output_path, "w", encoding="utf-8") as f:
-            f.write(pure_html_str)
-        logger.okay(f"  * {output_path}", verbose=self.verbose)
-        self.client.stop_client()
+            original_path = url_to_path(url=url, output_ext=".otml")
+        else:
+            output_path = Path(output_path)
+            original_path = output_path.with_suffix(".otml")
+
+        if load_original and original_path.exists():
+            html_str = self.load_html_str(original_path)
+        else:
+            self.client.start_client()
+            html_str = self.client.get_url_html(url)
+            self.client.stop_client()
+
+        if keep_original and (not load_original or not original_path.exists()):
+            # `not load_original` means force fetch page, so need to save it
+            self.save_html_str(html_str, original_path)
+
+        if is_purify:
+            pure_html_str = purify_html_str(
+                html_str, url=url, keep_href=True, output_format=output_format
+            )
+            self.save_html_str(pure_html_str, output_path)
 
 
 class UrlPagerArgParser(argparse.ArgumentParser):
@@ -45,6 +72,8 @@ class UrlPagerArgParser(argparse.ArgumentParser):
         self.add_argument("-n", "--output-name", type=str)
         self.add_argument("-e", "--output-ext", type=str)
         self.add_argument("-m", "--output-format", type=str, default="html")
+        self.add_argument("-l", "--load-original", action="store_true")
+        self.add_argument("-k", "--keep-original", action="store_true")
         self.args, _ = self.parse_known_args()
 
 
@@ -114,10 +143,17 @@ def run_url_pager():
     client = ChromeClientByConfig()
     pager = UrlPager(client=client, verbose=True)
     output_path, output_format = cli_args_to_output_path_format(args)
-    pager.fetch_url(url=args.url, output_path=output_path, output_format=output_format)
+    pager.fetch_url(
+        url=args.url,
+        output_path=output_path,
+        output_format=output_format,
+        load_original=args.load_original,
+        keep_original=args.keep_original,
+    )
 
 
 if __name__ == "__main__":
     run_url_pager()
 
     # python -m webu.workers.page -r "./data/htmls" -x "http://127.0.0.1:11111" -i "https://arxiv.org/abs/1810.04805"
+    # python -m webu.workers.page -l -k -x "http://127.0.0.1:11111" -i "https://arxiv.org/abs/1810.04805"
