54f1404ba1c1343860aa3f03db59b42c00776e28
Hansimov <591172499@qq.com>
2026-01-10 07:08:24 +0800

    :zap: [Enhance] add setup_swagger_ui to __init__

--- src/webu/__init__.py
+++ src/webu/__init__.py
@@ -5,3 +5,4 @@ from .searches.google import GoogleSearchConfigType, GoogleSearcher
 from .searches.weibo import WeiboSearchConfigType, WeiboSearcher
 from .ipv6.client import IPv6DBClient
 from .ipv6.session import IPv6Session
+from .fastapis.styles import setup_swagger_ui
